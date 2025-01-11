34e5d5407e41a1830074788477722674588e44df
sergeybabann <sergeybabann@yandex.ru>
2025-01-11 18:07:12 +0300

    сборка complete, отрабатывает исправно

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-const { src, dest, watch, parallel } = require('gulp')
+const { src, dest, watch, parallel, series } = require('gulp')
 const scss = require('gulp-sass')(require('sass'))
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify-es').default
 const browserSync = require('browser-sync').create()
+const autoprefixer = require('gulp-autoprefixer')
+const clean = require('gulp-clean')
 
 function styles() {
   return src('app/scss/style.scss')
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
     .pipe(concat('style.min.css'))
     .pipe(scss({ style: 'compressed' }))
     .pipe(dest('app/css'))
@@ -13,7 +16,7 @@ function styles() {
 }
 
 function scripts() {
-  return src('app/js/main.js')
+  return src(['node_modules/swiper/swiper-bundle.js', 'app/js/main.js'])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
@@ -34,9 +37,20 @@ function browsersync() {
   })
 }
 
+function cleanDist() {
+  return src('dist').pipe(clean())
+}
+
+function building() {
+  return src(['app/css/style.min.css', 'app/js/main.min.js', 'app/**/*.html'], {
+    base: 'app',
+  }).pipe(dest('dist'))
+}
+
 exports.styles = styles
 exports.scripts = scripts
 exports.watching = watching
 exports.browsersync = browsersync
 
+exports.build = series(cleanDist, building)
 exports.default = parallel(styles, scripts, browsersync, watching)
